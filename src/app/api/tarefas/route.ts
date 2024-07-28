ea715c80bea7e1e6194d4c18948fbe419d445f3d
Alex Nascimento <alexjose68@hotmail.com>
2024-07-28 15:58:08 -0300

    feat: criando uma rota para o método PUT para podermos atualizar um item da nossa lista

--- src/app/api/tarefas/route.ts
+++ src/app/api/tarefas/route.ts
@@ -13,3 +13,19 @@ export async function POST(request: Request) {
 
   return NextResponse.json(tarefas);
 }
+
+// Rota de atualizar uma tarefa única
+// http://localhost:3000/api/tarefas?index=1
+export async function PUT(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const index = searchParams.get("index");
+
+  const data = await request.json();
+
+  // console.log("Data: " + data.name);
+  // console.log("index: " + index);
+
+  tarefas[Number(index)] = data.name;
+
+  return NextResponse.json(tarefas);
+}
