d9adc12163dd5e6d419d16a207fef99a07420196
Alex Nascimento <alexjose68@hotmail.com>
2024-07-28 14:23:25 -0300

    feat: criei uma rota do tipo POST para poder inserir dados no meu array/lista tarefas

--- src/app/api/tarefas/route.ts
+++ src/app/api/tarefas/route.ts
@@ -5,3 +5,11 @@ let tarefas = ['Comprar coca', 'Estudar NextJS']
 export async function GET(request: Request) {
   return NextResponse.json(tarefas);
 }
+
+export async function POST(request: Request) {
+  const data = await request.json();
+  // console.log(data);
+  tarefas.push(data.name);
+
+  return NextResponse.json(tarefas);
+}
